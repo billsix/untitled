ddfb9744226389347c103572075c18b8f2dd1f09
William Emerison Six <billsix@gmail.com>
2018-12-29 18:56:41 -0500

    Added in defs required for equality.

--- Untitled.hs
+++ Untitled.hs
@@ -28,24 +28,105 @@
 {-[preface]-}
 {-= Preface-}
 
+
 {-[source,Haskell,linenums]-}
 {------}
 module Untitled where
 {------}
 
+{-[source,Haskell,linenums]-}
+{------}
+import Prelude hiding (subtract, and)
+{------}
+
+
+{-[source,Haskell]-}
+{------}
+{-data Boolean = True | False-}
+{-     deriving (Show)-}
+{------}
+
+
+{-[source,Haskell]-}
+{------}
+and :: (Bool -> (Bool -> Bool))
+and True True   = True
+and True False  = False
+and False True  = False
+and False False = True
+{------}
+
+{-= Equality-}
+
+
+{-For all data types created which are to be compared for-}
+{-equality, the data type must be an instance of the-}
+{-Eq class, which requires a == b to be defined.-}
+
+{-In order to allow say equal expressions of type Foo may be-}
+{-freely between equations, we make constraints on the definitions-}
+{-of equality.-}
+
+{-Reflexivity means for elements a of type Foo,-}
+
+{-[source,Haskell]-}
+{------}
+{-a == a-}
+{------}
+
+{-evaluates to True.-}
+
+{-Symmetry means for all elements a and b of type Foo,-}
+
+{-[source,Haskell]-}
+{------}
+{-(a == b) == (b == a)-}
+{------}
+
+{-evaluates to True.-}
+
+{-Transitivity means for all elements a, b, and c of type Foo,-}
+
+{-[source,Haskell]-}
+{------}
+{-  if and(a == b, b == c)-}
+{-   then a == c-}
+{-   else True-}
+{------}
+
+{-evaluates to True.-}
+
+{-[source,Haskell]-}
+{------}
+{-instance Eq Boolean where-}
+{-         (==) = booleanEqual where-}
+{-              booleanEqual True  True  = True-}
+{-              booleanEqual True  False = False-}
+{-              booleanEqual False True  = False-}
+{-              booleanEqual False False = True-}
+{------}
+
+{-== Substitiuon-}
+
+
+{-= Integers-}
 
 {-[source,Haskell,linenums]-}
 {------}
 data PeanoInteger = Zero | Succ PeanoInteger
      deriving (Show)
+{------}
 
 
+
+{-[source,Haskell,linenums]-}
+{------}
 instance Eq PeanoInteger where
          (==) = peanoIntegerEqual where
               peanoIntegerEqual Zero Zero         = True
               peanoIntegerEqual Zero (Succ a)     = False
-              peanoIntegerEqual (Succ b) Zero     = False
-              peanoIntegerEqual (Succ c) (Succ d) = peanoIntegerEqual c d
+              peanoIntegerEqual (Succ a) Zero     = False
+              peanoIntegerEqual (Succ a) (Succ b) = peanoIntegerEqual a b
 {------}
 
 {-[source,Haskell,linenums]-}
@@ -60,9 +141,26 @@ add a (Succ b) = Succ (add a b)
 multiply :: (PeanoInteger -> (PeanoInteger -> PeanoInteger))
 multiply a Zero = Zero
 multiply Zero b = Zero
-multiply a (Succ Zero) = a
 multiply a (Succ b) = add a (multiply a b)
 {------}
+
+
+{-= Plots-}
+
+{-= Enumerations-}
+
+{-[source,Haskell,linenums]-}
+{------}
+instance Enum PeanoInteger where
+         fromEnum = peanoIntegerFromEnum where
+                  peanoIntegerFromEnum Zero = 0
+                  peanoIntegerFromEnum (Succ x) = (peanoIntegerFromEnum x) + 1
+         toEnum = peanoIntegerToEnum where
+                  peanoIntegerToEnum 0 = Zero
+                  peanoIntegerToEnum x = Succ (peanoIntegerToEnum (x - 1))
+{------}
+
+{-= Signed Integers-}
 
 
 {-[source,Haskell,linenums]-}
@@ -91,9 +189,9 @@ toSigned a = Positive a
 {-[source,Haskell,linenums]-}
 {------}
 subtract :: (PeanoInteger -> (PeanoInteger -> SignedInteger))
-subtract a Zero = Positive a
-subtract Zero a = Negative a
-subtract (Succ a) (Succ b) = Untitled.subtract a b
+subtract a Zero            = Positive a
+subtract Zero a            = Negative a
+subtract (Succ a) (Succ b) = subtract a b
 {------}
 
 
@@ -104,8 +202,8 @@ subtract (Succ a) (Succ b) = Untitled.subtract a b
 {------}
 signedIntegerAdd :: (SignedInteger -> (SignedInteger -> SignedInteger))
 signedIntegerAdd (Positive a) (Positive b) = Positive (add a b)
-signedIntegerAdd (Positive a) (Negative b) = Untitled.subtract a b
-signedIntegerAdd (Negative a) (Positive b) = Untitled.subtract b a
+signedIntegerAdd (Positive a) (Negative b) = subtract a b
+signedIntegerAdd (Negative a) (Positive b) = subtract b a
 signedIntegerAdd (Negative a) (Negative b) = Negative (add a b)
 {------}
 
@@ -133,14 +231,6 @@ signedIntegerMultiply (Negative a) (Positive b) = Negative (multiply a b)
 signedIntegerMultiply (Negative a) (Negative b) = Positive (multiply a b)
 {------}
 
+{-= Rationals-}
 
-{-[source,Haskell,linenums]-}
-{------}
-instance Enum PeanoInteger where
-         fromEnum = peanoIntegerFromEnum where
-                  peanoIntegerFromEnum Zero = 0
-                  peanoIntegerFromEnum (Succ x) = (peanoIntegerFromEnum x) + 1
-         toEnum = peanoIntegerToEnum where
-                  peanoIntegerToEnum 0 = Zero
-                  peanoIntegerToEnum x = Succ (peanoIntegerToEnum (x - 1))
-{------}
+{-= Intervals-}
